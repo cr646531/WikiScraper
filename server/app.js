874ee4104aab2eab6f1b8658ee43d0033a1a2d1f
Charlie <cr646531@gmail.com>
2020-01-14 11:53:27 -0500

    added route to create two queues - one for all links on start page and one for all links on destination page

--- server/app.js
+++ server/app.js
@@ -2,10 +2,52 @@ const express = require('express');
 const app = express();
 const axios = require('axios');
 const cheerio = require('cheerio');
+const { createQueue } = require('./funcs.js');
 
 module.exports = app;
 
+/* Helper Function */
+
+const fetchData = async (url) => {
+    const result = await axios.get(url);
+    return cheerio.load(result.data);
+};
+
+/* Routes */
 
 app.get('/', async (req, res) => {
     res.send('test');
+});
+
+app.get('/links', async (req, res) => {
+    var startUrl = 'https://en.wikipedia.org/wiki/History';
+    var endUrl = 'https://en.wikipedia.org/wiki/Physics';
+
+    var startQueue = [];
+    var endQueue = [];
+    var temp;
+    
+    // grab the html for the start page
+    var $ = await fetchData(startUrl);
+
+    // create array of all the paragraphs on the page
+    var paragraphs = $('p', '.mw-parser-output');
+
+    // use the paragraphs array to find the links and add them to the queue
+    temp = createQueue(paragraphs, startUrl);
+    startQueue = startQueue.concat(temp);
+
+    // grab the html for the destination page
+    var $ = await fetchData(endUrl);
+
+    // create an array of all the paragraphs on the page
+    var paragraphs = $('p', '.mw-parser-output');
+
+    // use the paragraphs array to find the links on the page and add them to the queue
+    var temp = createQueue(paragraphs, endUrl);
+    endQueue = endQueue.concat(temp);
+
+    
+    res.send(endQueue);
+
 });
